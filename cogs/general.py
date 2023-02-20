0746ade363aad9b4f77d38d9f2e494e679e44701
Vien Catigan <vien@courvix.com>
2023-02-20 09:27:39 +0000

    Fixed with await/async on setup and removed unused imports

--- cogs/general.py
+++ cogs/general.py
@@ -1,9 +1,5 @@
+from discord.ext import commands
 import discord
-from discord.ext import commands, tasks
-import random
-import os
-
-import config # Your Bot Config File
 
 class general(commands.Cog):
     def __init__(self, bot):
@@ -13,5 +9,5 @@ class general(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
 
-def setup(bot):
-    bot.add_cog(general(bot))
+async def setup(bot):
+    await bot.add_cog(general(bot))
